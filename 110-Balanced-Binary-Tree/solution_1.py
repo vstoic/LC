867f68764e0d2cc95c68a08b48053619ebd0ba8f
Sing Cheng <Sing.cheng@outlook.com>
2024-03-31 16:05:27 -0400

    110

---
+++ 110-Balanced-Binary-Tree/solution_1.py
@@ -0,0 +1,9 @@
+def isBalanced(root):
+    def dfs(root):
+        if not root:
+            return [True, 0] 
+        left = dfs(root.left)
+        right = dfs(root.right)
+        balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+        return [balanced, max(left[1], right[1]) + 1]
+    return dfs(root)[0]
